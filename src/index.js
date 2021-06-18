a8ac03bbc9746441c4320784981521e80c85267e
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './re_';
-export * from './regex_indexOf';
-export * from './regex_lastIndexOf';
-//# sourceMappingURL=src/index.js.map
--- src/re_.js
+++
@@ -1,4 +0,0 @@
-export function re_(pattern, flags) {
-    return new RegExp(pattern, flags);
-}
-//# sourceMappingURL=src/re_.js.map
